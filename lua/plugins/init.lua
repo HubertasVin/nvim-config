c93b5a225a5fc62794d0bb08e0991058cc6e57d3
HubertasVin <hubertas.personal@proton.me>
2025-08-28 11:32:41 +0200

    Fixed treesitter errors while undoing or pasting code.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -39,6 +39,9 @@ return {
         "vimdoc",
       },
     },
+    highlight = { enable = true, additional_vim_regex_highlighting = false },
+    autotag = { enable = true },
+    indent = { enable = true },
   },
 
   {
@@ -113,9 +116,5 @@ return {
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
-  },
-  {
-    "towolf/vim-helm",
-    event = { "BufReadPre", "BufNewFile" },
   },
 }
