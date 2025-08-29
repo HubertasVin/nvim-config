4fcbf0a3e5028eae8bcfa98b6eec71eb5d363817
HubertasVin <hubertas.personal@proton.me>
2025-08-29 14:01:50 +0200

    Renamed import variables; Outsourced blink close functionality.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,16 +1,17 @@
 require "nvchad.mappings"
 
-local hover = require "utils.hover"
-local def = require "utils.definition"
-local sn = require "utils.section_nav"
+local util_hover = require "utils.hover"
+local util_def = require "utils.definition"
+local util_blink = require "utils.blink"
+local util_sn = require "utils.section_nav"
+local util_git = require "utils.git"
 local gs = require "gitsigns"
-local gsec = require "utils.git_section"
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- LSP related mappings with descriptions
-map("n", "K", hover.hover_with_diagnostics, { desc = "Code Description and diagnostics", table.unpack(opts) })
-map("n", "<leader>cd", def.definition_popup, { desc = "Code Go to definition", table.unpack(opts) })
+map("n", "K", util_hover.hover_with_diagnostics, { desc = "Code Description and diagnostics", table.unpack(opts) })
+map("n", "<leader>cd", util_def.definition_popup, { desc = "Code Go to definition", table.unpack(opts) })
 map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action", table.unpack(opts) })
 map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Code Rename", table.unpack(opts) })
 map("n", "<leader>cu", require("telescope.builtin").lsp_references, { noremap = true, silent = true })
@@ -19,19 +20,7 @@ map("n", "<leader>cu", require("telescope.builtin").lsp_references, { noremap = 
 map("n", "<leader>cf", require("conform").format, { desc = "Format File code", table.unpack(opts) })
 
 -- Close blink completion, if it is visible
-map("i", "<Esc>", function()
-  local ok, blink = pcall(require, "blink.cmp")
-  if ok and blink and (blink.is_visible and blink.is_visible()) then
-    if blink.cancel then
-      blink.cancel()
-    end
-    if blink.hide then
-      blink.hide()
-    end
-    return ""
-  end
-  return vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
-end, { expr = true, silent = true, desc = "Close completion if visible, else Esc" })
+map("i", "<Esc>", util_blink.close, { expr = true, silent = true, desc = "Blink Close completion" })
 
 -- Open CMD line without holding shift
 map("n", ";", ":")
@@ -53,8 +42,8 @@ map("v", "<", "<gv")
 map("v", ">", ">gv")
 
 -- Improved "{" and "}" to move to a non blank line, instead of a blank line
-map({ "n", "x", "o" }, "{", sn.section_nav_fn "prev_start", { desc = "Move cursor to block start" })
-map({ "n", "x", "o" }, "}", sn.section_nav_fn "next_end", { desc = "Move cursor to block end" })
+map({ "n", "x", "o" }, "{", util_sn.section_nav_fn "prev_start", { desc = "Move cursor to block start" })
+map({ "n", "x", "o" }, "}", util_sn.section_nav_fn "next_end", { desc = "Move cursor to block end" })
 
 -- Search and replace
 map(
@@ -84,6 +73,6 @@ map(
 
 -- Git
 map("n", "<leader>gd", gs.preview_hunk_inline, { desc = "Git Hunk (section) diff", table.unpack(opts) })
-map("n", "<leader>gr", gsec.reset_current_line, { desc = "Git Current line reset", table.unpack(opts) })
-map("v", "<leader>gr", gsec.reset_selected_lines, { desc = "Git Selected lines reset", table.unpack(opts) })
+map("n", "<leader>gr", util_git.reset_current_line, { desc = "Git Current line reset", table.unpack(opts) })
+map("v", "<leader>gr", util_git.reset_selected_lines, { desc = "Git Selected lines reset", table.unpack(opts) })
 map("n", "<leader>gR", ":Gitsigns reset_hunk<CR>", { desc = "Git Hunk (section) revert", table.unpack(opts) })
---
+++ lua/utils/blink.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+function M.close()
+  local ok, blink = pcall(require, "blink.cmp")
+  if ok and blink and (blink.is_visible and blink.is_visible()) then
+    if blink.cancel then
+      blink.cancel()
+    end
+    if blink.hide then
+      blink.hide()
+    end
+    return ""
+  end
+  return vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+end
+
+return M
