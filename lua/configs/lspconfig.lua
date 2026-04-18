022639073e09e82c6a9835f190da8ce99117e132
HubertasVin <hubertas.personal@proton.me>
2026-04-18 21:30:55 +0300

    Fixing outdated lspconfig.

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -104,14 +104,8 @@ local servers = {
 }
 
 for server_name, config in pairs(servers) do
-	-- NEWER WAY with lsp.config and lsp.enable.
-	-- doesn't always work yet though.
-	-- E.g. javascript rename capability missing from additions.
-	-- vim.lsp.config(server_name, config)
-	-- vim.lsp.enable(server_name)
-	-- OLDER WAY:
-	local merged = vim.tbl_deep_extend("force", defaults, config)
-	lspconfig[server_name].setup(merged)
+	vim.lsp.config(server_name, config)
+	vim.lsp.enable(server_name)
 end
 
 -- Global default border for all LSP float windows
